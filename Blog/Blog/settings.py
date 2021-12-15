4125d50df9e16da3ba659d12de4a31c830e02b66
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-12-15 16:55:56 +0200

    permissions added with postgresql DB, the data of books rendering correctly.But only authorized users can modify it

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -86,6 +86,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgresql',
+#         'USER': 'postgresql',
+#         'PASSWORD': '1996',
+#         'HOST': 'db',
+#         'PORT': 5432
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -134,6 +145,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ]
 }
